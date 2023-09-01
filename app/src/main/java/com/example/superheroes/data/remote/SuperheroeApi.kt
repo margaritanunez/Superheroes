fbec47d46ff79736d56293810c482863624449d5
Margaret <maenunezt@gmail.com>
2023-09-01 00:38:55 -0400

    se incluye una suspend fun getDataDetailSuperheroe en SuperheroeApi.kt, para obtener la información detallada del superheroe seleccionado dentro de la app

--- app/src/main/java/com/example/superheroes/data/remote/SuperheroeApi.kt
+++ app/src/main/java/com/example/superheroes/data/remote/SuperheroeApi.kt
@@ -2,9 +2,13 @@ package com.example.superheroes.data.remote
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface SuperheroeApi {
 
     @GET("superheroes/")
     suspend fun getDataSuperheroe(): Response<List<Superheroe>>
+
+    @GET("superheroes/{id}")
+    suspend fun getDataDetailSuperheroe(@Path("id") id: Int): Response<DetailSuperheroe>
 }
