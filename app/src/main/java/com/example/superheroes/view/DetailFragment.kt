938138ac9573ad70706a674447d834ddaa9b4fda
Margaret <maenunezt@gmail.com>
2023-09-01 03:03:52 -0400

    se asocian los botones a las variables para que muestren la información en el fragmento detail

--- app/src/main/java/com/example/superheroes/view/DetailFragment.kt
+++ app/src/main/java/com/example/superheroes/view/DetailFragment.kt
@@ -5,28 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import coil.load
 import com.example.superheroes.R
+import com.example.superheroes.databinding.FragmentDetailBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_PARAM1 = "id"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var id: Int = 0
+    private lateinit var binding: FragmentDetailBinding
+    private val superheroeViewModel: SuperheroeViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            id = it.getInt(ARG_PARAM1)
         }
     }
 
@@ -35,26 +32,28 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        superheroeViewModel.getDetailsSuperheroeViewModel(id)
+        initListener()
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun initListener() {
+        superheroeViewModel.detailSuperheroeLiveData(id).observe(viewLifecycleOwner){
+            if (it!= null){
+                binding.imgSuperheroeDetail.load(it.imageLink)
+                binding.tvColourDetail.text = it.colour
+                binding.tvNameDetail.text = it.name
+                binding.tvOriginDetail.text = it.origin
+                binding.tvPower.text = it.power
+                binding.tvYearDetail.text = it.yearCreation.toString()
+                if (!it.translation) {
+                    binding.tvTranslationDetail.text = "Sin traducción"
+                } else {
+                    binding.tvTranslationDetail.text = "Cuenta con traducción al español"
                 }
             }
+        }
     }
+
 }
